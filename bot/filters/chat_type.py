9cc29f3b55c2a1e09172039684eb18798e261b1a
SivkaBulka <SivkaBulka.ru@gmail.com>
2026-05-15 01:30:07 +0300

    Update chat_type.py

--- bot/filters/chat_type.py
+++ bot/filters/chat_type.py
@@ -1,1 +1,17 @@
+from aiogram.filters import Filter
+from aiogram.types import Message, CallbackQuery
 
+class GroupChatFilter(Filter):
+    """Разрешает только групповые чаты (включая супергруппы)"""
+    async def __call__(self, message: Message) -> bool:
+        return message.chat.type in ("group", "supergroup")
+
+class PrivateChatFilter(Filter):
+    """Разрешает только личные сообщения"""
+    async def __call__(self, message: Message) -> bool:
+        return message.chat.type == "private"
+
+class GroupChatCallbackFilter(Filter):
+    """Для колбэков: проверяет, что сообщение из группы"""
+    async def __call__(self, callback: CallbackQuery) -> bool:
+        return callback.message.chat.type in ("group", "supergroup")
